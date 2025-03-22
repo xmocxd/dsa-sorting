53775ba31dd8b962988784fa2afc49ef255db647
mocxd <mocxd.a@gmail.com>
2025-03-21 22:12:14 -0400

    selection sort

--- insertion.js
+++ insertion.js
@@ -1,8 +1,4 @@
 function insertionSort(arr) {
-    if (arr.length <= 1) {
-        return arr;
-    }
-
     for (let p = 1; p < arr.length; p++) {
         for (let i = 0; i < p; i++) {
             if (arr[p] < arr[i]) {
@@ -14,7 +10,6 @@ function insertionSort(arr) {
     }
 
     return arr;
-
 }
 
 console.log('[0,-1,99,2,4,77,22,-999]\n' + insertionSort([0,-1,99,2,4,77,22,-999]));
--- selection.js
+++ selection.js
@@ -1,3 +1,22 @@
-function selectionSort() {}
+function selectionSort(arr) {
+    for (let o = 0; o < arr.length; o++) {
+        let min = o;
+        for (let i = o + 1; i < arr.length; i++) {
+            if (arr[i] < arr[min]) {
+                min = i;
+            }
+        }
+
+        if (min !== o) {
+            let t = arr[o];
+            arr[o] = arr[min];
+            arr[min] = t;
+        }
+    }
+
+    return arr;
+}
+
+console.log('[4, 20, 12, 10, 7, 9]\n' + selectionSort([4, 20, 12, 10, 7, 9]));
 
 module.exports = selectionSort;
