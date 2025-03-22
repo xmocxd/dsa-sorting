682c78f6ee930d8dcc9a6b9e6faf1110fe5fb562
mocxd <mocxd.a@gmail.com>
2025-03-21 20:28:00 -0400

    insertion sort

--- insertion.js
+++ insertion.js
@@ -1,3 +1,22 @@
-function insertionSort() {}
+function insertionSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    for (let p = 1; p < arr.length; p++) {
+        for (let i = 0; i < p; i++) {
+            if (arr[p] < arr[i]) {
+                let t = arr[i];
+                arr[i] = arr[p];
+                arr[p] = t;
+            }
+        }
+    }
+
+    return arr;
+
+}
+
+console.log('[0,-1,99,2,4,77,22,-999]\n' + insertionSort([0,-1,99,2,4,77,22,-999]));
 
 module.exports = insertionSort;
