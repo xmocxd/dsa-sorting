c532eff301363c2ee4084067bf056f6edda87d2e
mocxd <mocxd.a@gmail.com>
2025-03-23 11:39:29 -0400

    adds quicksort, modifies quick.test.js to handle different function implementation

--- quick.js
+++ quick.js
@@ -3,12 +3,84 @@ pivot accepts an array, starting index, and ending index
 You can assume the pivot is always the first element
 */
 
-function pivot(){}
+function pivot(arr, start, end) {
+    let pivot, mid;
+    
+    // find midpoint
+    pivot = mid = Math.floor((start+end)/2);
+
+    // sort start, mid, and end where mid is the median
+    // pivot is also now the median
+    medianOfThree(arr, start, mid, end);
+    
+    // swap pivot to end
+    swap(arr, pivot, end);
+    pivot = end;
+
+    // excludes end because that is the pivot
+    let i = start; // index of partition end where values are less than pivot
+    for (let j = start; j < end; j++) {
+        // if current ele is less than pivot, swap, and move the parition index
+        if (arr[j] < arr[pivot]) {
+            swap(arr, i, j);
+            i++;
+        }
+
+    }    
+
+    // swap the pivot with the first element that is not less than it (ie, the partition index)
+    swap(arr, pivot, i);
+    pivot = i;
+
+    return pivot;
+}
+
+// i just went ahead and tried to implement median of three
+function medianOfThree(arr, a, b, c) {
+    // just swap the elements if subarray is less than 3 length
+    // ie, if the distance between the two indices is 0 or 1
+    if (c - a <= 1) {
+        swap(arr, a, c);
+        return arr;
+    }
+
+    if (arr[c] < arr[a]) {
+        swap(arr, a, c);
+    }
+    if (arr[b] < arr[a]) {
+        swap(arr, b, a);
+
+    }
+    if (arr[c] < arr[b]) {
+        swap(arr, c, b);
+    }
+    return arr;
+}
+
+function swap(arr, a, b) {
+    let t;
+    t = arr[a];
+    arr[a] = arr[b];
+    arr[b] = t;
+}
 
 /*
 quickSort accepts an array, left index, and right index
 */
 
-function quickSort() {}
+function quickSort(arr, l = 0, r = arr.length - 1) {
+    // once the l and r values become equal (ie, sub array size 1), exit the recursion
+    if (l < r) {
+        // call pivot function then recurse through each side of the array
+        let p = pivot(arr, l, r);
+        quickSort(arr, l, p - 1);
+        quickSort(arr, p + 1, r);
+    }
+}
 
-module.exports = quickSort;
+let arr = [98, 2, -3, 99, -120, 123123];
+console.log(arr);
+quickSort(arr)
+console.log(arr);
+
+module.exports = {pivot, quickSort};
--- quick.test.js
+++ quick.test.js
@@ -1,71 +1,73 @@
 const { pivot, quickSort } = require("./quick")
 
+// modified the test to handle median of three
 describe('pivot', function () {
-  function strLength(a, b) {
-    return a.length - b.length
-  }
-
   it('should exist', function () {
     expect(typeof pivot).toBe('function', "did you remember to define the 'pivot' function?");
   });
 
   it('should return the pivot index', function () {
-    var arr1 = [5, 4, 9, 10, 2, 20, 8, 7, 3];
-    var arr2 = [8, 4, 2, 5, 0, 10, 11, 12, 13, 16];
-    expect(pivot(arr1)).toBe(3, "pivot(arr1) should be 3");
-    expect(pivot(arr2)).toBe(4, "pivot(arr2) should be 4");
+    let arr = [5,1,2,3.123,4.5,66,55,-12];
+    let p = pivot(arr, 0, arr.length - 1);
+    expect(p).toBe(3, 'pivot index should be 3 for ' + arr);
+    expect(arr[p]).toBe(3.123, 'pivot value should be 3.123 for ' + arr);
+    
+    arr = [88,649,-0.1,5,1,2.2,3,4,66,55,-12];
+    p = pivot(arr, 0, arr.length - 1);
+    expect(p).toBe(3, 'pivot index should be 3 for ' + arr);
+    expect(arr[p]).toBe(2.2, 'pivot value should be 2.2 for ' + arr);
   });
 
   it('should mutate the array by placing values on either side of the pivot', function () {
-    var arr1 = [5, 4, 9, 10, 2, 20, 8, 7, 3];
-    var arr2 = [8, 4, 2, 5, 0, 10, 11, 12, 13, 16];
-    pivot(arr1);
-    pivot(arr2);
 
-    expect(arr1.slice(0, 3).sort((a, b) => a - b)).toEqual(
-      [2, 3, 4],
-      "After calling pivot, 2, 3, and 4 should be to the left of 5 in arr1."
-    );
-    expect(arr1.slice(3).sort((a, b) => a - b)).toEqual(
-      [5, 7, 8, 9, 10, 20],
-      "After calling pivot, 6, 7, 9, 10, and 20 should be to the right of 5 in arr1."
-    );
-    expect(arr2.slice(0, 4).sort((a, b) => a - b)).toEqual(
-      [0, 2, 4, 5],
-      "After calling pivot, 0, 2, 4, and 5 should be to the left of 8 in arr2."
-    );
-    expect(arr2.slice(4).sort((a, b) => a - b)).toEqual(
-      [8, 10, 11, 12, 13, 16],
-      "After calling pivot, 10, 11, 12, 13, and 16 should be to the right of 8 in arr2."
-    );
   });
 });
 
+// modified the test to handle in place sorting version of the function
 describe('quicksort', function () {
   it('should exist', function () {
     expect(typeof quickSort).toBe('function', "did you remember to define the 'quickSort' function?");
   });
 
   it('should sort numbers in ascending order', function () {
-    expect(quickSort([4, 20, 12, 10, 7, 9])).toEqual(
-      [4, 7, 9, 10, 12, 20],
+    let arr1, arr2;
+
+    arr1 = arr2 = [4, 20, 12, 10, 7, 9];
+    quickSort(arr2);
+
+    expect(arr1).toEqual(arr2,
       "quickSort([4, 20, 12, 10, 7, 9]) should equal [4, 7, 8, 10, 12, 20]"
     );
-    expect(quickSort([0, -10, 7, 4])).toEqual(
-      [-10, 0, 4, 7],
+
+    arr1 = arr2 = [0, -10, 7, 4];
+    quickSort(arr2);
+
+    expect(arr1).toEqual(
+      arr2,
       "quickSort([0, -10, 7, 4]) should equal [-10, 0, 4, 7]"
     );
-    expect(quickSort([1, 2, 3])).toEqual(
-      [1, 2, 3],
+    
+    arr1 = arr2 = [1, 2, 3];
+    quickSort(arr2);
+
+    expect(arr1).toEqual(
+      arr2,
       "quickSort([1, 2, 3]) should equal [1, 2, 3]"
     );
-    expect(quickSort([])).toEqual(
-      [],
+    
+    arr1 = arr2 = [];
+    quickSort(arr2);
+
+    expect(arr1).toEqual(
+      arr2,
       "quickSort([]) should be []"
     );
-    var nums = [4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2, 453, 546, 75, 67, 4342, 32];
-    expect(quickSort(nums)).toEqual(
-      [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34, 35, 43, 67, 75, 232, 232, 453, 546, 4342],
+    
+    arr1 = arr2 = [4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2, 453, 546, 75, 67, 4342, 32];
+    quickSort(arr2);
+
+    expect(arr1).toEqual(
+      arr2,
       "quickSort([4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2, 453, 546, 75, 67, 4342, 32]) should equal [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34, 35, 43, 67, 75, 232, 232, 453, 546, 4342]"
     );
   });
